5ebcd870189a2fc6e9699b73c6725f28f7d5944d
sfd4ni <danielbarrosorocio@gmail.com>
2025-02-14 20:45:00 +0000

    resources renamed

--- src/main.js
+++ src/main.js
@@ -10,12 +10,12 @@ const videos = ["https://www.youtube.com/embed/WEC52d9dpBQ?si=QR-F9syuvd7FHDQ_",
   "https://www.youtube.com/embed/CIZD8etf51U?si=cESEj0R5lIG_noiO", "https://www.youtube.com/embed/lOlRVvXMmfY?si=XinHgodQTqnP_lYS",
   "https://www.youtube.com/embed/o8PdBKNr62A?si=mFjOuuZlMaSx4_Et", "https://www.youtube.com/embed/QUKXHH30JQs?si=ulxtRLrM9oOS0Eic"
 ]
-const imagenes = ["Safe_and_Sound.png", "Fix_You.png", "sunday_morning.jpg", "linger.png", "Duvet.png", "dango_daikazoku.png", "theylongtobe.png",
-  "Snake_Eater.png", "yellow.png", "do_you_think_im_sexy.jpg", "something_stupid.png", "iris.png", "wake_me_up.jpg", "viva_la_vida.jpg", "alright.png",
-  "Youre_Gonna_Go_Far_Kid.png"];
-const imagenesOG = ["Safe_and_Sound_og.jpg", "Fix_You_og.jpg", "sunday_morning_og.jpg", "linger_og.jpg", "Duvet_og.jpg", "dango_daikazoku_og.jpg", "theylongtobeog.jpg",
-  "Snake_Eater_og.jpg", "yellow_og.jpg", "do_you_think_im_sexy_og.jpg", "something_stupid_og.jpeg", "iris_og.jpg", "wake_me_up_og.jpg", "viva_la_vida_og.jpg", "alright_og.jpg",
-  "Youre_Gonna_Go_Far_Kid_og.jpg"];
+const imagenes = ["safe_and_sound.png", "fix_you.png", "sunday_morning.jpg", "linger.png", "duvet.png", "dango_daikazoku.png", "theylongtobe.png",
+  "snake_eater.png", "yellow.png", "do_you_think_im_sexy.jpg", "something_stupid.png", "iris.png", "wake_me_up.jpg", "viva_la_vida.jpg", "alright.png",
+  "you_are_gonna_go_far_kid.png"];
+  const imagenesog = ["safe_and_sound_og.jpg", "fix_you_og.jpg", "sunday_morning_og.jpg", "linger_og.jpg", "duvet_og.jpg", "dango_daikazoku_og.jpg", "they_long_to_be_og.jpg",
+    "snake_eater_og.jpg", "yellow_og.jpg", "do_you_think_im_sexy_og.jpg", "something_stupid_og.jpeg", "iris_og.jpg", "wake_me_up_og.jpg", "viva_la_vida_og.jpg", "alright_og.jpg",
+    "you_are_gonna_go_far_kid_og.jpg"];
 const artistas = ["NameLater", "NameLater", "Sahelanthropus", "Ryoshi", "Gestalt", "NameLater", "Ryoshi",
   "NameLater", "Nini IV", "Sahelanthropus", "Ryoshi", "NameLater", "Arteries", "Sahelanthropus", "Nini IV",
   "NameLater"];
