032a04e04a11f3d2b1b0913e72baa104c56dfced
sfd4ni <danielbarrosorocio@gmail.com>
2025-02-18 05:06:54 +0000

    Added songs and a first slide with credits

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
-// Number of slides (You can change this to 50 or any other number)
-const totalSlides = 30;
+
 
 /*const videos = ["https://www.youtube.com/embed/WEC52d9dpBQ?si=QR-F9syuvd7FHDQ_", "https://www.youtube.com/embed/wbTrL_ME7-k?si=idJ74ZuMMHxR0hky",
   "https://www.youtube.com/embed/-dsJjxjEyfo?si=CYhaAb7Nvj8sXIGb", "https://www.youtube.com/embed/kG_Dxawxmxk?si=UCjdyA-AhRoWEEa4",
@@ -11,7 +10,7 @@ const totalSlides = 30;
   "https://www.youtube.com/embed/ZW73eFqXa9Q?si=4hOsORcV0bkVaz4T", "https://www.youtube.com/embed/QUKXHH30JQs?si=ulxtRLrM9oOS0Eic"
 ]*/
 
-const videos = ["https://www.youtube.com/embed/PXxeydkPS9I?si=tLlxbi1nonpMYui", "https://www.youtube.com/embed/sMj9-qR-aKI?si=xQ2Eivz8e_qV3_iR",
+const videos = ["", "https://www.youtube.com/embed/PXxeydkPS9I?si=tLlxbi1nonpMYui", "https://www.youtube.com/embed/sMj9-qR-aKI?si=xQ2Eivz8e_qV3_iR",
   "https://www.youtube.com/embed/Pj4xUnAK8c0?si=_Im-2Py8EltUvtxR", "https://www.youtube.com/embed/1VgeRW19NnU?si=Wa2OfcyB-qbcNGsz",
   "https://www.youtube.com/embed/GNResRKDOdQ?si=2YuwGYiV5gB4lGzw", "https://www.youtube.com/embed/8dOPVTk5eQQ?si=o9VAsb1rYCY2qup8",
   "https://www.youtube.com/embed/Bv8L-9CdVco?si=52LmgneIpvd6NRxB", "https://www.youtube.com/embed/5Zok5Ctejak?si=eqx4RYTkv1YXFKN7", 
@@ -23,42 +22,53 @@ const videos = ["https://www.youtube.com/embed/PXxeydkPS9I?si=tLlxbi1nonpMYui", 
   "https://www.youtube.com/embed/hxPaJvm3Br0?si=KK7Osj0LijnLxASb", "https://www.youtube.com/embed/e1WmkydX7e0?si=ZnvYmKHR0AwpnjUn",
   "https://www.youtube.com/embed/D9e_Q9J1Xpk?si=DsSVOxoPkR6GC4BT", "https://www.youtube.com/embed/YzqpHsKYdRs?si=fKrJYuVHdg8ySGoS",
   "https://www.youtube.com/embed/oxvzB5gPaY4?si=eWeMc-qTws_T32VL", "https://www.youtube.com/embed/clK3-XhtzTE?si=vmiys1NEvdkUkOyr", 
-  "https://www.youtube.com/embed/tUEy0GYa7rQ?si=TH7j1reMvGhnEFPB",
-
+  "https://www.youtube.com/embed/tUEy0GYa7rQ?si=TH7j1reMvGhnEFPB", "https://www.youtube.com/embed/xBfcv5bbgSw?si=n5VxYE_3pmROKJO0",
+  "https://www.youtube.com/embed/CG4Fw0UjRi4?si=zwVFjB2DbHnc6qfq", "https://www.youtube.com/embed/qvpDD5r_ig0?si=uytpd1_h47PSsvtW",
+  "https://www.youtube.com/embed/1ju3_OT80y4?si=QPvqrefhM5sqo3xF", "https://www.youtube.com/embed/VpVEHD0349s?si=lD5U37wpkL59x2Qj", 
+  "https://www.youtube.com/embed/1NUtPXgYZdU?si=x5M0r9sqqIADeb3L", "https://www.youtube.com/embed/CFdEOXA5KIc?si=NyfuTVqvg4rZd4lO",
+  "https://www.youtube.com/embed/FOH2uMIXWlg?si=zNFQ475zhaucyXBe", "https://www.youtube.com/embed/Pg0LHfT1Duk?si=L1mgSTm84F1gi2Zj",
+  "https://www.youtube.com/embed/znkIJoZRnDc?si=QvQoOiZ1u6mBzWab"
 ]
 
-const imagenes = ["safe_and_sound.png", "fix_you.png", "sunday_morning.jpg", "linger.png", "duvet.png", "dango_daikazoku.png", "they_long_to_be.png",
+const imagenes = ["", "safe_and_sound.png", "fix_you.png", "sunday_morning.jpg", "linger.png", "duvet.png", "dango_daikazoku.png", "they_long_to_be.png",
   "snake_eater.png", "yellow.png", "do_you_think_im_sexy.jpg", "something_stupid.png", "iris.png", "wake_me_up.jpg", "viva_la_vida.jpg", "alright.png",
   "you_are_gonna_go_far_kid.png", "505.png", "bakamitai.png", "counting_stars.png", "creep.png", "feel_good.jpg", "renai_circulation.png", "stay_with_me.png",
-  "stereo_hearts.png", "your_graduation.jpg", "telephone_number.png", "california_girls.png", "dancing_queen.png", "lovefool.png" ];
+  "stereo_hearts.png", "your_graduation.jpg", "telephone_number.png", "california_girls.png", "dancing_queen.png", "lovefool.png", "beyond_the_sea.png", 
+  "dream_a_little_dream_of_me.png", "my_way.png", "ruler_of_everything.png", "alone_again.png", "starman.png", "honey_bee.png"];
 
-  const imagenesOg = ["safe_and_sound_og.jpg", "fix_you_og.jpg", "sunday_morning_og.jpg", "linger_og.jpg", "duvet_og.jpg", "dango_daikazoku_og.jpg", "they_long_to_be_og.jpg",
+  const imagenesOg = ["credits_art.png", "safe_and_sound_og.jpg", "fix_you_og.jpg", "sunday_morning_og.jpg", "linger_og.jpg", "duvet_og.jpg", "dango_daikazoku_og.jpg", "they_long_to_be_og.jpg",
     "snake_eater_og.jpg", "yellow_og.jpg", "do_you_think_im_sexy_og.jpg", "something_stupid_og.jpeg", "iris_og.jpg", "wake_me_up_og.jpg", "viva_la_vida_og.jpg", "alright_og.jpg",
     "you_are_gonna_go_far_kid_og.jpg", "505_og.jpg", "bakamitai_og.png", "counting_stars_og.jpg", "creep_og.jpg", "feel_good_og.jpg", "renai_circulation_og.jpg", "stay_with_me_og.jpg",
-  "stereo_hearts_og.jpg", "your_graduation_og.jpg", "telephone_number_og.jpg", "california_girls_og.png", "dancing_queen_og.jpg", "lovefool_og.jpg"];
+  "stereo_hearts_og.jpg", "your_graduation_og.jpg", "telephone_number_og.jpg", "california_girls_og.png", "dancing_queen_og.jpg", "lovefool_og.jpg","beyond_the_sea.png", 
+  "dream_a_little_dream_of_me.png", "my_way_og.jpg", "ruler_of_everything_og.jpg", "alone_again_og.jpg", "starman_og.png", "honey_bee_og.jpg"];
 
-const artistas = ["NameLater", "NameLater", "Sahelanthropus", "Ryoshi", "Gestalt", "NameLater", "Ryoshi",
+const artistas = ["", "NameLater", "NameLater", "Sahelanthropus", "Ryoshi", "Gestalt", "NameLater", "Ryoshi",
   "NameLater", "Nini IV", "Sahelanthropus", "Ryoshi", "NameLater", "Arteries", "Sahelanthropus", "Nini IV",
   "NameLater", "NameLater", "Ryoshi", "NameLater", "NameLater", "Arteries", "Ryoshi", "Ryoshi",
-  "NameLater", "Arteries", "Ryoshi", "Gestalt", "Ryoshi", "NameLater"];
+  "NameLater", "Arteries", "Ryoshi", "Gestalt", "Ryoshi", "NameLater", "NameLater", 
+  "NameLater", "NameLater", "Gestalt", "NameLater", "Gestalt", "NameLater"];
 
-const songTitles = ["Safe and Sound", "Fix You", "Sunday Morning", "Linger", "Duvet", "Dango Daikazoku", "(They Long to Be) Close to You",
+const songTitles = ["", "Safe and Sound", "Fix You", "Sunday Morning", "Linger", "Duvet", "Dango Daikazoku", "(They Long to Be) Close to You",
     "Snake Eater", "Yellow", "Do you think I'm sexy?", "Somethin' Stupid", "Iris", "Wake Me Up", "Viva la Vida", "Alright",
     "You Are Gonna Go Far, Kid", "505", "Bakamitai", "Counting Stars", "Creep", "Feel Good Inc.", "Renai Circulation", "Stay With Me",
-  "Stereo Hearts", "Your Graduation", "Telephone Number", "California Girls", "Dancing Queen", "Lovefool"];
+  "Stereo Hearts", "Your Graduation", "Telephone Number", "California Girls", "Dancing Queen", "Lovefool", "Beyond The Sea", 
+  "Dream A Little Dream Of Me", "My Way", "Ruler Of Everything", "Alone Again (Naturally)", "Starman", "Honeybee"];
 
-const originalSongArtists = ["Capital Cities", "Coldplay", "Maroon5", "The Cranberries", "Bôa", "Chata", "The Carpenters",
-  "Cynthia Harrell", "Coldplay", "Rod Stewart", "Frank & Nancy Sinatra", "City of Angels: Music from the Motion Picture", "Green Day", "Coldplay", "Kendrick Lamar",
+const originalSongArtists = ["", "Capital Cities", "Coldplay", "Maroon5", "The Cranberries", "Bôa", "Chata", "The Carpenters",
+  "Cynthia Harrell", "Coldplay", "Rod Stewart", "Frank & Nancy Sinatra", "Goo Goo Dolls", "Green Day", "Coldplay", "Kendrick Lamar",
   "The Offspring", "Arctic Monkeys", "Takaya Kuroda", "OneRepublic", "Radiohead", "Gorillaz", "Kana Hanazawa", "Miki Matsubara",
-  "Gym Class Heroes", "Modern Baseball", "Junko Ōhashi", "Katy Perry", "Abba", "The Cardigans"];
+  "Gym Class Heroes", "Modern Baseball", "Junko Ōhashi", "Katy Perry", "Abba", "The Cardigans", "Bobby Darin", 
+  "Ella Fitzgerald", "Frank Sinatra", "Tally Hall", "Gilbert O'Sullivan", "David Bowie", "Steam Powered Giraffe"];
 
-const albumNames = ["In a Tidal Wave of Mystery", "X&Y", "Songs About Jane", "Everybody Else Is Doing It, So Why Can't We?", "Twilight", "Dango Daikazoku", "Close to You",
-  "Metal Gear Solid 3: Original Soundtrack", "Parachutes", "Blondes Have More Fun", "The World We Knew", "NameLater", "American Idiot", "Viva la Vida or Death and All His Friends", "To Pimp a Butterfly",
+const albumNames = ["", "In a Tidal Wave of Mystery", "X&Y", "Songs About Jane", "Everybody Else Is Doing It, So Why Can't We?", "Twilight", "Dango Daikazoku", "Close to You",
+  "Metal Gear Solid 3: Original Soundtrack", "Parachutes", "Blondes Have More Fun", "The World We Knew", "City of Angels: Music from the Motion Picture", "American Idiot", "Viva la Vida or Death and All His Friends", "To Pimp a Butterfly",
   "Rise and Fall, Rage and Grace", "Favourite Worst Nightmare", "Baka Mitai", "Native", "Pablo Honey", "Demon Days", "Bakemonogatari Ongaku Zenshū Songs & Soundtracks", "Pocket Park",
-  "The Papercut Chronicles II", "You're Gonna Miss It All", "Tea For Tears", "Teenage Dream", "Arrival", "First Band on the Moon"];
+  "The Papercut Chronicles II", "You're Gonna Miss It All", "Tea For Tears", "Teenage Dream", "Arrival", "First Band on the Moon","Don't Worry Be Jazzy By Bobby Darin", 
+  "Dream A Little Dream Of Me, Vol. 1", "My Way", "Marvin's Marvelous Mechanical Museum", "Himself", "The Rise and Fall of Ziggy Stardust and the Spiders from Mars", "The 2¢ Show"];
 
-// Array to store slide data (just using placeholder images for this example)
-const slideData = Array.from({ length: totalSlides }, (_, i) => ({
+const totalSlides = imagenes.length - 1;
+
+const slideData = Array.from({ length: totalSlides + 1 }, (_, i) => ({
   id: i,
   imageOgUrl: imagenesOg[i],
   imageUrl: imagenes[i],
@@ -77,6 +87,7 @@ function createCarouselItems() {
   carousel.innerHTML = ''; // Clear existing items
 
   slideData.forEach(item => {
+    
     const contentSlide = document.createElement('div');
     contentSlide.classList.add('content-slide');
     const bgSlide = document.createElement('div');
@@ -88,89 +99,136 @@ function createCarouselItems() {
     
     
     
+          
+    if (item.imageOgUrl == "credits_art.png") {
+      // Image with text
+      const creditsArt = document.createElement('div');
+      creditsArt.classList.add('credits-art');
+      const imageText = document.createElement('img');
+      //const bgSlide = document.createElement('div');
+      //contentSlide.classList.add('bg-slide');
+      if (typeof item.imageOgUrl !== 'undefined') {  
+        imageText.src = `./resources/${item.imageOgUrl}`;
+        slide.style=`background-image: url('./resources/${item.imageOgUrl}')`;
+      }
+
+      const textCreditos = document.createElement('div');
+      textCreditos.classList.add('text-credits');
+
+      const titulo = document.createElement('h1');
+      titulo.textContent = 'POKO KARAOKE CREDITS'
+      const participantsList = document.createElement('ul');
+
+      const artistas = document.createElement('li');
+      artistas.textContent = "THANKS TO THE ARTISTS WHO MADE ALL OF THIS POSSIBLE: Ryoshi, Sigma Of Gestalt, I Will Think About My Name Later, Sahelanthropus, Arteries";
+
+      const recopilador = document.createElement('li');
+      recopilador.textContent = "Recopilation of songs and video editing: Cherro";
+
+      const organizador = document.createElement('li');
+      organizador.textContent = "Organization and Web Dev: Dani";
+
+      const others = document.createElement('li');
+      others.textContent = "Also thanks to all the fandits for submitting what songs they would like to see on here!";
+
+
+      participantsList.appendChild(artistas);
+      participantsList.appendChild(recopilador);
+      participantsList.appendChild(organizador);
+      participantsList.appendChild(others);
+      textCreditos.appendChild(titulo);
+      textCreditos.appendChild(participantsList);
+      creditsArt.appendChild(imageText);
+      contentSlide.appendChild(creditsArt);
+      contentSlide.append(textCreditos);
+      //contentSlide.appendChild(originalImageDiv);
+      //slide.appendChild(bgSlide);
+      bgSlide.appendChild(contentSlide);
+      slide.appendChild(bgSlide);
+      carousel.appendChild(slide);
+    } else {
+      // Image with text
+      const pokoCoverArt = document.createElement('div');
+      pokoCoverArt.classList.add('poko-cover-art');
+      const imageText = document.createElement('img');
+      //const bgSlide = document.createElement('div');
+      //contentSlide.classList.add('bg-slide');
+      if (typeof item.imageUrl !== 'undefined') {  
+        imageText.src = `./resources/${item.imageUrl}`;
+        slide.style=`background-image: url('./resources/${item.imageUrl}')`;
+      }
+
+      const textPoko = document.createElement('div');
+      textPoko.classList.add('text-poko');
+      textPoko.textContent = item.originalArtistText;
+      
+
+      // Column 2: YouTube video and image with text
+      // YouTube video
+      const videoContainer = document.createElement('div');
+      videoContainer.classList.add('video-container');
+      const iframe = document.createElement('iframe');
+      if (typeof item.youtubeUrl !== 'undefined') {
+        iframe.src = item.youtubeUrl;
+
+      }
+      iframe.frameborder = "0";
+      iframe.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
+      iframe.allowfullscreen = true;
+      videoContainer.appendChild(iframe);
+
+      // Column 2: Image
+      const originalImageDiv = document.createElement('div');
+      const originalImageDivFila2 = document.createElement('div');
+      originalImageDiv.classList.add('original-image-div');
+      originalImageDivFila2.classList.add('original-image-div-fila2');
+
+      const image = document.createElement('img');
+      if (typeof item.imageOgUrl !== 'undefined') {  
+        image.src = `./resources/${item.imageOgUrl}`;
+      }
+      
+      const textOriginal = document.createElement('div');
+      textOriginal.classList.add('text-original');
+      const propertiesList = document.createElement('ul');
+
+      const songTitle = document.createElement('li');
+      songTitle.textContent = item.songTitle;
+
+      const originalSongArtist = document.createElement('li');
+      originalSongArtist.textContent = item.originalSongArtist;
+
+      const albumName = document.createElement('li');
+      albumName.textContent = item.albumName;
+
+      const artist = document.createElement('li');
+      artist.textContent = item.originalArtistText;
+
+      propertiesList.appendChild(songTitle);
+      propertiesList.appendChild(originalSongArtist);
+      propertiesList.appendChild(albumName);
+      propertiesList.appendChild(artist);
+
+
+      originalImageDiv.appendChild(videoContainer);
+      originalImageDivFila2.appendChild(image);
+      originalImageDivFila2.appendChild(textOriginal);
+      originalImageDiv.appendChild(originalImageDivFila2);
+      
+      textOriginal.appendChild(propertiesList);
+      pokoCoverArt.appendChild(imageText);
+      //pokoCoverArt.appendChild(textPoko);
+      
+      //slide.appendChild(videoContainer);
+      //slide.appendChild(bgSlide);
+      contentSlide.appendChild(pokoCoverArt);
+      contentSlide.appendChild(originalImageDiv);
+      //slide.appendChild(bgSlide);
+      bgSlide.appendChild(contentSlide);
+      slide.appendChild(bgSlide);
+      carousel.appendChild(slide);
     
-    
-    
-    // Image with text
-    const pokoCoverArt = document.createElement('div');
-    pokoCoverArt.classList.add('poko-cover-art');
-    const imageText = document.createElement('img');
-    //const bgSlide = document.createElement('div');
-    //contentSlide.classList.add('bg-slide');
-    if (typeof item.imageUrl !== 'undefined') {  
-      imageText.src = `./resources/${item.imageUrl}`;
-      slide.style=`background-image: url('./resources/${item.imageUrl}')`;
     }
-
-    const textPoko = document.createElement('div');
-    textPoko.classList.add('text-poko');
-    textPoko.textContent = item.originalArtistText;
-    
-
-    // Column 2: YouTube video and image with text
-    // YouTube video
-    const videoContainer = document.createElement('div');
-    videoContainer.classList.add('video-container');
-    const iframe = document.createElement('iframe');
-    if (typeof item.youtubeUrl !== 'undefined') {
-      iframe.src = item.youtubeUrl;
-
-    }
-    iframe.frameborder = "0";
-    iframe.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
-    iframe.allowfullscreen = true;
-    videoContainer.appendChild(iframe);
-
-    // Column 2: Image
-    const originalImageDiv = document.createElement('div');
-    const originalImageDivFila2 = document.createElement('div');
-    originalImageDiv.classList.add('original-image-div');
-    originalImageDivFila2.classList.add('original-image-div-fila2');
-
-    const image = document.createElement('img');
-    if (typeof item.imageOgUrl !== 'undefined') {  
-      image.src = `./resources/${item.imageOgUrl}`;
-    }
-    
-    const textOriginal = document.createElement('div');
-    textOriginal.classList.add('text-original');
-    const propertiesList = document.createElement('ul');
-
-    const songTitle = document.createElement('li');
-    songTitle.textContent = item.songTitle;
-
-    const originalSongArtist = document.createElement('li');
-    originalSongArtist.textContent = item.originalSongArtist;
-
-    const albumName = document.createElement('li');
-    albumName.textContent = item.albumName;
-
-    const artist = document.createElement('li');
-    artist.textContent = item.originalArtistText;
-
-    propertiesList.appendChild(songTitle);
-    propertiesList.appendChild(originalSongArtist);
-    propertiesList.appendChild(albumName);
-    propertiesList.appendChild(artist);
-
-
-    originalImageDiv.appendChild(videoContainer);
-    originalImageDivFila2.appendChild(image);
-    originalImageDivFila2.appendChild(textOriginal);
-    originalImageDiv.appendChild(originalImageDivFila2);
-    
-    textOriginal.appendChild(propertiesList);
-    pokoCoverArt.appendChild(imageText);
-    //pokoCoverArt.appendChild(textPoko);
-    
-    //slide.appendChild(videoContainer);
-    //slide.appendChild(bgSlide);
-    contentSlide.appendChild(pokoCoverArt);
-    contentSlide.appendChild(originalImageDiv);
-    //slide.appendChild(bgSlide);
-    bgSlide.appendChild(contentSlide);
-    slide.appendChild(bgSlide);
-    carousel.appendChild(slide);
   });
 }
 
